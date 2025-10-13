04ea088f4d04f4cea2cf0d715d10f5c80620c241
Eren <erenpenan@gmail.com>
2025-10-13 11:19:44 +0800

    fix terraform formating on file main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,29 +1,29 @@
 terraform {
-    required_providers {
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "~> 3.0"
-        }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "docker" {}
 
 resource "docker_image" "node_demo" {
-    name = "node-demo-local:latest"
+  name = "node-demo-local:latest"
 
-    build {
-        context = "${path.module}/../app"
-        dockerfile = "${path.module}/../app/Dockerfile"
-    }
+  build {
+    context    = "${path.module}/../app"
+    dockerfile = "${path.module}/../app/Dockerfile"
+  }
 }
 
 resource "docker_container" "node_demo" {
-    name = "node-demo-container"
-    image = docker_image.node_demo.name
-    ports {
-        internal = 8080
-        external = 8080
-    }
+  name  = "node-demo-container"
+  image = docker_image.node_demo.name
+  ports {
+    internal = 8080
+    external = 8080
+  }
 }
 
