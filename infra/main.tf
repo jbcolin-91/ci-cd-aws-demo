7342c1d8ebf5f1dc3e27b1d54f63c4bac1b9324f
Eren <erenpenan@gmail.com>
2025-10-14 14:55:47 +0800

    Split the deploy in plan then apply for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -9,21 +9,52 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "node_demo" {
-  name = "node-demo-local:latest"
+# Variables
 
-  build {
-    context    = "${path.module}/../app"
-    dockerfile = "${path.module}/../app/Dockerfile"
-  }
+variable "docker_username" {
+    description = "Docker Hub username"
+    type        = string
 }
 
-resource "docker_container" "node_demo" {
+variable "image_name" {
+    description = "Repository name"
+    type        = string
+    default     = "ci-cd-demo-app"
+}
+
+variable "image_tag" {
+    description = "Image tag to deploy"
+    type        = string
+    default     = "latest"
+}
+
+# Resources
+
+resource "docker_image" "app" {
+  name         = "${var.docker_username}/${var.image_name}:${var.image_tag}"
+  keep_locally = false
+}
+
+resource "docker_container" "app" {
   name  = "node-demo-container"
-  image = docker_image.node_demo.name
+  image = docker_image.app.name
+
   ports {
     internal = 8080
     external = 8080
   }
+
+  must_run = true
+  restart  = "unless-stopped"
+}
+
+# Outputs
+
+output "app_image" {
+    value = docker_image.app.name
+}
+
+output app_url {
+    value = "http://localhost:8080"
 }
 
